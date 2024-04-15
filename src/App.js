b118ce7afa3fde1141b9c39829d6e3cc000182f1
Pawan Bharti <bpawan96@gmail.com>
2024-04-16 02:08:10 +0530

    Importing and Exporting Components named & default export

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-// import logo from './logo.svg';
-// import './App.css';
+import Gallery from "./Gallery.js";
+import Profile from "./Profile.js";
 
 function formatDate(date) {
   return date.toLocaleDateString();
 }
 
-function App(props) {
+export default function App(props) {
   return (
     <div className="Comment">
       <div className="UserInfo">
@@ -22,24 +22,10 @@ function App(props) {
       <div className="Comment-date">
         {formatDate(props.date)}
       </div>
+      <Profile />,
+      <Gallery />
     </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     {/* <img src={logo} className="App-logo" alt="logo" /> */}
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
-export default App;
+// export default App;
